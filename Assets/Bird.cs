c1e63ab0bbaf0a112fe53a5bb4111aeccca6a4a1
unknown <mpcondo@yahoo.com>
2022-05-15 09:45:45 -0400

    initial commit w/ background, greenbird, and basic collider

---
+++ Assets/Bird.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = newPosition;
+    }
+}
